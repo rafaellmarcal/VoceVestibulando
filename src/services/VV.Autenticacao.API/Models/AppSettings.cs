f17ad54ddf3989123d374c7b21b2b7d8d488e964
Rafael Lima Marçal <rafaell.lima92@gmail.com>
2022-01-12 23:15:57 -0400

    Configurando JWT.

---
+++ src/services/VV.Autenticacao.API/Models/AppSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace VV.Autenticacao.API.Models
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+        public int ExpiracaoHoras { get; set; }
+        public string Emissor { get; set; }
+        public string ValidoEm { get; set; }
+    }
+}
--- src/services/VV.Autenticacao.API/Startup.cs
+++ src/services/VV.Autenticacao.API/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
@@ -5,9 +6,12 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System;
+using System.Text;
 using VV.Autenticacao.API.Context;
+using VV.Autenticacao.API.Models;
 
 namespace VV.Autenticacao.API
 {
@@ -28,6 +32,32 @@ namespace VV.Autenticacao.API
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<AutenticacaoDbContext>()
                 .AddDefaultTokenProviders();
+
+            var appSettingsSection = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = true;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidIssuer = appSettings.Emissor,
+                    ValidAudience = appSettings.ValidoEm
+                };
+            });
 
             services.AddControllers();
 

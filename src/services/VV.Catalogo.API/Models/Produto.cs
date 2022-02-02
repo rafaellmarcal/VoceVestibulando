6324af8405f79f86eb731fe4c71c4d317ae14917
Rafael Lima Marçal <rafaell.lima92@gmail.com>
2022-02-01 23:45:43 -0400

    Catalogo API.

---
+++ src/services/VV.Catalogo.API/Models/Produto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using VV.Core.DomainObjects;
+
+namespace VV.Catalogo.API.Models
+{
+    public class Produto : Entity
+    {
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public bool Ativo { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public string Imagem { get; set; }
+        public int QuantidadeEstoque { get; set; }
+    }
+}
---
+++ src/services/VV.Catalogo.API/Startup.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace VV.Catalogo.API
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IHostEnvironment environment)
+        {
+            var configurationBuilder = new ConfigurationBuilder()
+                .SetBasePath(environment.ContentRootPath)
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{environment.EnvironmentName}.json", true, true)
+                .AddEnvironmentVariables();
+
+            if (environment.IsDevelopment())
+                configurationBuilder.AddUserSecrets<Startup>();
+
+            Configuration = configurationBuilder.Build();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}

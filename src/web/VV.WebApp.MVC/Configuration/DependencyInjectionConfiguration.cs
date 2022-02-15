2d3b09949053a8a582a3b1ac88ead7136b687387
Rafael Lima Marçal <rafaell.lima92@gmail.com>
2022-02-14 22:30:04 -0400

    polly.

--- src/web/VV.WebApp.MVC/Configuration/DependencyInjectionConfiguration.cs
+++ src/web/VV.WebApp.MVC/Configuration/DependencyInjectionConfiguration.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Polly;
+using Polly.Extensions.Http;
+using Polly.Retry;
+using System;
+using System.Net.Http;
 using VV.WebApp.MVC.Middlewares;
 using VV.WebApp.MVC.Models.User;
 using VV.WebApp.MVC.Services;
@@ -16,13 +21,35 @@ namespace VV.WebApp.MVC.Configuration
             services.AddHttpClient<IAuthService, AuthService>();
 
             services.AddHttpClient<ICatalogService, CatalogService>()
-                .AddHttpMessageHandler<HttpClientAuthorizationMiddleware>();
+                .AddHttpMessageHandler<HttpClientAuthorizationMiddleware>()
+                .AddPolicyHandler(PollyExtensions.WaitAndRetryPolicy())
+                .AddTransientHttpErrorPolicy(p => p.CircuitBreakerAsync(5, TimeSpan.FromSeconds(15)));
 
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
             services.AddScoped<IUserAuthenticated, UserAuthenticated>();
 
             return services;
+        }
+    }
+
+    public static class PollyExtensions
+    {
+        public static AsyncRetryPolicy<HttpResponseMessage> WaitAndRetryPolicy()
+        {
+            return HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .WaitAndRetryAsync(new[] {
+                    TimeSpan.FromSeconds(1),
+                    TimeSpan.FromSeconds(5),
+                    TimeSpan.FromSeconds(10)
+                },
+                (outcome, timespan, retryCount, context) =>
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine($"Attempt number: {retryCount}");
+                    Console.ForegroundColor = ConsoleColor.White;
+                });
         }
     }
 }
